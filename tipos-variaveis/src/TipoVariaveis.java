909276dc34522a643ce3a5c9bc05a1e429633d81
Lima <jvbarb8@gmail.com>
2024-06-04 00:32:37 -0300

    atualizaçao de aprendizado em java

--- tipos-variaveis/src/TipoVariaveis.java
+++ tipos-variaveis/src/TipoVariaveis.java
@@ -1,5 +1,21 @@
 public class TipoVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos 
+        //estudar a class string que representa texto na aplicaçao
+        
+        String meuNome = "Victor Lima";
+        
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
     }
 }
